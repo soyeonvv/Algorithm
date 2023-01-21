9ae05b5e5e5124c16d6e884af1e782cd091bb7a0
soyeonvv <thdusdlvv@naver.com>
2023-01-22 03:41:42 +0900

    [2579] 계단 오르기

---
+++ BOJ/DP/2579.py
@@ -0,0 +1,14 @@
+n = int(input())
+array = [int(input()) for _ in range(n)]
+
+dp = [0] * n
+dp[0] = array[0]
+if n >= 2:
+    dp[1] = array[0] + array[1]
+if n >=3:
+    dp[2] = max(array[0] + array[2], array[1] + array[2])
+
+for i in range(3, n):
+    dp[i] = max(dp[i -3] + array[i - 1] + array[i], dp[i - 2] + array[i])
+
+print(dp[n - 1])
