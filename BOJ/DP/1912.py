d61def7ae31af1c1176ff3bcb0e3caf240f636ff
soyeonvv <thdusdlvv@naver.com>
2023-03-16 01:26:58 +0900

    [1912] 연속합

---
+++ BOJ/DP/1912.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+array = list(map(int, input().split()))
+
+dp = [0] * n
+dp[0] = array[0]
+
+for i in range(1, n):
+    dp[i] = max(dp[i - 1] + array[i], array[i])
+
+print(max(dp))
