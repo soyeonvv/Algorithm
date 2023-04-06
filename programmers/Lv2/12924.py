219711c4579bef05d12d1c6f800cdda37788fa1e
soyeonvv <sovvworld@gmail.com>
2023-04-07 00:40:32 +0900

    숫자의 표현

---
+++ programmers/Lv2/12924.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 1
+
+    for i in range(1, n // 2 + 1):
+        stack = []
+        for j in range(i, n + 1):
+            stack.append(j)
+            if sum(stack) >= n:
+                break
+        if sum(stack) == n:
+            answer += 1
+
+    return answer
