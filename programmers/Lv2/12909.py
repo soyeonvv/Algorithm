10476107b2d65a4379876ad17d16f78fef9b84c8
soyeonvv <sovvworld@gmail.com>
2023-04-18 20:54:33 +0900

    올바른 괄호

---
+++ programmers/Lv2/12909.py
@@ -0,0 +1,17 @@
+def solution(s):
+    answer = True
+    stack = []
+
+    for c in s:
+        if c == '(':
+            stack.append(c)
+        else:
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+
+    if len(stack) != 0:
+        return False
+
+    return answer
