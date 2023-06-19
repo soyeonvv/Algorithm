619b2c7c857e6bd938fde8a2a7f62c621c5bcef8
soyeonvv <sovvworld@gmail.com>
2023-06-19 18:07:19 +0900

    [7568] 덩치

---
+++ BOJ/BruteForce/7568.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+people = []
+
+for _ in range(n):
+    w, h = map(int, input().split())
+    people.append((w, h))
+
+for i in people:
+    rank = 1
+    for j in people:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end=' ')
