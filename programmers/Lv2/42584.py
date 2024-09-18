d1b04b7a61f0ad05c4120dade2ac23edf125ccb6
soyeonvv <sovvworld@gmail.com>
2024-09-18 15:17:50 +0900

    주식가격

---
+++ programmers/Lv2/42584.py
@@ -0,0 +1,12 @@
+def solution(prices):
+    n = len(prices)
+    answer = [i for i in range(n - 1, -1, -1)]
+    
+    stack = [0]
+    for i in range(1, n):
+        while stack and prices[stack[-1]] > prices[i]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+        
+    return answer
